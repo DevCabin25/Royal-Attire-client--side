83f85360ef0159da07acbd334a4d83ec3ed8a18d
Asadulla56 <mdasadullahoque294@gmail.com>
2025-05-01 00:16:17 +0600

    about session done

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import About from './About';
 
 const Banner = () => {
     return (
         <div>
-            <h1 className="text-blue-300 text-center ">this is banner section</h1>
+            {/* <h1 className="text-blue-300 text-center ">this is banner section</h1> */}
+            <About></About>
+            
         </div>
+        
+        
     );
 };
 
