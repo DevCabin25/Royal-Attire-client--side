602291b6539b3fd65a38ea329b6d0d195ffec6fe
Asadulla56 <mdasadullahoque294@gmail.com>
2025-05-01 00:19:27 +0600

    about session update

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import About from './About';
 
 const Banner = () => {
     return (
         <div>
-            {/* <h1 className="text-blue-300 text-center ">this is banner section</h1> */}
-            <About></About>
+            
+        
             
         </div>
         
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Components/Home/Banner';
+import About from '../Components/Home/About';
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <About></About>
         </div>
     );
 };
