68c462e44df6272f3bfa6c0e74f2b8d050cde54e
Asadulla56 <mdasadullahoque294@gmail.com>
2025-04-30 19:17:28 +0600

    Footer session done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="text-center text-xl font-bold mt-10">
+      Welcome to Royal Attire!
+    </div>
+  );
+};
+
+export default App;
