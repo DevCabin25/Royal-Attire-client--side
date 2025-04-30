4895850bfc59bc41187bfe6bfb05e197d4bbad16
mubashirbappi <mubashirbappi@gmail.com>
2025-04-30 21:52:25 +0600

    fix bug

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your app content will be rendered by the router */}
+    </div>
+  );
+}
+
+export default App;
